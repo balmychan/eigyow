4bb9f536a87dd37a2e02028db54372025df53d98
prpr_man <oharada@live.jp>
2015-09-19 17:43:33 +0000

    Add save feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,10 +4,25 @@ class ArticlesController < ApplicationController
 
   def new
     @article = Article.new
-    @user = "Test"
+    @user = current_user
   end
   
   def create
+    company_id = Company.find_by_name(params[:company].to_s)
+    if company_id.blank?
+      company_id = Company.create(:name => params[:company].to_s).id
+    end
     
+    @article = Article.create(:note => params[:article][:note], :type => params[:type],
+      :user_id => params[:article][:user_id], :company_id => company_id)
+      
+    reps = params[:representative].split(',')
+    reps.each do |rep|
+      person = CompanyPerson.find_by_name(rep.to_s)
+      if person.blank?
+        CompanyPerson.create(:name => rep.to_s, :company_id => company_id)
+      end
+    end
+      
   end
 end
