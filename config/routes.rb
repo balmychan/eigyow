207a08ff3f3d56b1e0283651afb53d723ab24799
Takahashi kanji <kanjirz50@gmail.com>
2015-09-19 16:01:16 +0000

    create mypage

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get 'home/index'
 
   get 'home/show'
+  
+  get 'mypage/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
