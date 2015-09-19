b1c49cd311512d3a0c8f273d6fb4e79509b361ca
shizunaito <adfa1728@chiba-u.jp>
2015-09-19 16:07:11 +0000

    login signup UI change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 20150919095331) do
+
+ActiveRecord::Schema.define(version: 20150919140533) do
 
   create_table "article_keywords", force: :cascade do |t|
     t.string   "keyword",     limit: 255
@@ -61,9 +62,7 @@ ActiveRecord::Schema.define(version: 20150919095331) do
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
   end
-  
-ActiveRecord::Schema.define(version: 20150919140533) do
-  
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
@@ -77,19 +76,11 @@ ActiveRecord::Schema.define(version: 20150919140533) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "username",               limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "username"
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["username"], name: "index_users_on_username", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
