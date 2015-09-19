00ba454fd2d1cbebadddbf87131f0886348347be
shizunaito <adfa1728@chiba-u.jp>
2015-09-19 11:42:22 +0000

    make login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+        home_show_path
+  end
+
+  private
+        def sign_in_required
+            redirect_to new_user_session_url unless user_signed_in?
+        end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
