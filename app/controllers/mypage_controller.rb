589e3c632edd8bbb016d51a82055f16c7acbe002
Takahashi kanji <kanjirz50@gmail.com>
2015-09-20 00:42:54 +0000

    add the search function

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,4 +1,13 @@
 class MypageController < ApplicationController
   def index
+    @c_id = params[:c_id]
+    if @c_id.nil?
+        @c_name = "すべて"
+        @articles = Article.where(user_id: current_user).order("created_at DESC").page(params[:page])
+    else
+        @c_name = Company.where(id: @c_id).first.name
+        @articles = Article.where(user_id: current_user).where(company_id: @c_id).order("created_at DESC").page(params[:page])
+    end
+    
   end
 end
