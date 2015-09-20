fabbe6dde2f14a5c01f24dd641f9269378fdd4af
shizunaito <adfa1728@chiba-u.jp>
2015-09-20 07:02:43 +0000

    root changed

--- config/routes.rb
+++ config/routes.rb
@@ -74,5 +74,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  root to: "home#index"
+  root to: "articles#new"
 end
